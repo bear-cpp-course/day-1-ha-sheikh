873815f350f16f93bd143a66f4b89959deaacf10
Hamzah Sheikh <hamzahs@hotmail.co.uk>
2020-02-07 14:32:50 +0000

    First attempt at initialising integers and doubles

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 int main()
 {
-  //This is a comment
-  /* This is a multiline
-   comment*/
-  std::cout << "Hello world!\n";
+  int a{1};
+  a = 4;
+  std::cout << a << std::endl;
+
+  int b{a};
+  b = 3;
+  std::cout << b << std::endl;
+
+  double c{3};
+  std::cout << c << std::endl;
 }
