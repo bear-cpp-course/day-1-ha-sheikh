d3b516ba09c59c6b425ba5bc46c354b1eee2a8cf
Hamzah Sheikh <hamzahs@hotmail.co.uk>
2020-02-13 20:46:12 +0000

    Code to convert user input to uppercase and check for alphabetic input

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 int main()
 {
-  std::string msg {"Hello!"}; // This is how you initialise a string
-  std::cout << msg << "\n";
+  std::string in_str {""};
+  std::cout << "Enter string: ";
+  std::string out_str {""};
+  char in_char{'x'};
+  char out_char{'X'};
+  int i{0};
+  while(std::cin >> in_char)
+  {
+    if(isalpha(in_char))            // Check if character is alphabetic
+    {
+      if(!isupper(in_char))         // If it is alphabetic, check if it is lowercase
+      {
+        out_char = in_char - 32;    // Convert to uppercase
+        std::cout << out_char;
+        out_str[i] = out_char;
+        i++;
+        continue;
+      }
+      else
+      {
+      out_char = in_char;           // Otherwise leave it as it is
+      std::cout << out_char;
+      out_str[i] = out_char;
+      i++;
+      continue;
+      }
+    }
+    else
+    {
+      out_char = 'X';
+      std::cout << out_char;
+      out_str[i] = out_char;
+      i++;
+      continue;
+    }
+    std::cout << out_str << std::endl;
+  }
   return 0;
 }
